06ec2cb4857aae5f60ccc1e29fc5054c7f550455
galio <pu758614@gmail.com>
2020-02-06 02:35:45 +0800

    123

--- main.php
+++ main.php
@@ -53,7 +53,7 @@ foreach ($client->parseEvents() as $event) {
                     $status = 0;
                     // $data['sec'] ='';
                     //$data = cheack_arrange($message['text']);
-                    pr($message['text'])
+                    pr($message['text']);
             //         if($data['error'] == '1'){
             //             $client->reply_text($event['replyToken'],$data['msg']);
             //             write_log($guestdata['displayName'],$event['source']['userId'],$message['text'],'0');
