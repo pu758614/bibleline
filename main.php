e3be259f8026baab75f0346f4efda56aa24e2c51
galio <pu758614@gmail.com>
2020-02-06 01:18:53 +0800

    123

--- main.php
+++ main.php
@@ -24,104 +24,79 @@ $data = $client->parseEvents();
 
 foreach ($client->parseEvents() as $event) {
     $guestdata = getGuestInfo($channelAccessToken,$channelSecret,$event['source']['userId']);
-    $log_msg .= json_encode();
-    file_put_contents('test.log', $log_msg,FILE_APPEND);
-    $client->replyMessage([
-                        'replyToken' => $event['replyToken'],
-                        'messages' => [
-                            [
-                                "type"=>"location",
-                                "title"=>"灣告輝底家啦！！",
-                                "address"=>"813左營區重立路61號",
-                                "latitude"=>'22.673217',
-                                "longitude"=>'120.313176'
+
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    if($message['text']=='三民聖教會'){
+                        $client->replyMessage([
+                            'replyToken' => $event['replyToken'],
+                            'messages' => [
+                                [
+                                    "type"=>"location",
+                                    "title"=>"灣告輝底家啦！！",
+                                    "address"=>"813左營區重立路61號",
+                                    "latitude"=>'22.673217',
+                                    "longitude"=>'120.313176'
+                                ]
                             ]
-                        ]
-                    ]);
-    // $client->replyMessage([
-    //                     'replyToken' => $event['replyToken'],
-    //                     'messages' => [
-    //                         [
-    //                             "type"=>"location",
-    //                             "title"=>"灣告輝底家啦！！",
-    //                             "address"=>"813左營區重立路61號",
-    //                             "latitude"=>'22.673217',
-    //                             "longitude"=>'120.313176'
-    //                         ]
-    //                     ]
-    //                 ]);
-    // switch ($event['type']) {
-    //     case 'message':
-    //         $message = $event['message'];
-    //         switch ($message['type']) {
-    //             case 'text':
-    //                 if($message['text']=='三民聖教會'){
-    //                     $client->replyMessage([
-    //                         'replyToken' => $event['replyToken'],
-    //                         'messages' => [
-    //                             [
-    //                                 "type"=>"location",
-    //                                 "title"=>"灣告輝底家啦！！",
-    //                                 "address"=>"813左營區重立路61號",
-    //                                 "latitude"=>'22.673217',
-    //                                 "longitude"=>'120.313176'
-    //                             ]
-    //                         ]
-    //                     ]);
-    //                     write_log($guestdata['displayName'],$event['source']['userId'],$message['text'],'4');
-    //                     break;
-    //                 }
-    //                 $comman_key =array('?','這到底怎麼用啦','目錄','舊約','新約','我要抽');
-    //                 if(in_array($message['text'],$comman_key)){
-    //                     write_log($guestdata['displayName'],$event['source']['userId'],'comman-'.$message['text'],'4');
-    //                     exit;
-    //                 }
-    //                 $status = 0;
-    //                 $data['sec'] ='';
-    //                 $data = cheack_arrange($message['text']);
-    //                 if($data['error'] == '1'){
-    //                     $client->reply_text($event['replyToken'],$data['msg']);
-    //                     write_log($guestdata['displayName'],$event['source']['userId'],$message['text'],'0');
-    //                     exit;
-    //                 }
-    //                 if($data['type'] == 'search' ){
-    //
-    //                     $data['sec'] = isset($data['sec'])?$data['sec']:'';
-    //                     $results = search($data['book'],$data['chap'],$data['sec'],$message['text']);
-    //
-    //                     if($results['error']!='1' && $results['status']=='1'){
-    //                         $text_arr = text_change_arr($results['data']);
-    //                         $client->reply_text_arr($event['replyToken'],$text_arr);
-    //                     }else if($results['error']!='1' && $results['status']=='2'){
-    //                         $client->reply_text($event['replyToken'],$results['data']);
-    //                     }
-    //
-    //                 }else if($data['type'] == 'kw' ||$data['type'] == 'kwf'){
-    //                     $results = search_keyword($data['kw'],$data['type']);
-    //                     if($results['status']=='ok'){
-    //                         $status ='1';
-    //                     }else if($results['status']=='error'){
-    //                         $status = '6';
-    //                     }else{
-    //                         $status = '1';
-    //                     }
-    //                         $client->reply_text($event['replyToken'],$results['msg']);
-    //                 }else{
-    //                     $text = '意料以外的錯誤，請麻煩通知開發人一下！'.emoji('10007D');
-    //                     $client->reply_text($event['replyToken'],$text);
-    //                     $status = '6';
-    //                 }
-    //                 write_log($guestdata['displayName'],$event['source']['userId'],$message['text'],$status);
-    //                 break;
-    //             default:
-    //                 write_log($guestdata['displayName'],$event['source']['userId'],'Unsupported message type-'.$message['type'],'5');
-    //                 error_log('Unsupported message type: ' . $message['type']);
-    //                 break;
-    //         }
-    //         break;
-    //     default:
-    //         write_log($guestdata['displayName'],$event['source']['userId'],'Unsupported event type-'.$event['type'],'5');
-    //         error_log('Unsupported event type: ' . $event['type']);
-    //         break;
-    // }
+                        ]);
+                        write_log($guestdata['displayName'],$event['source']['userId'],$message['text'],'4');
+                        break;
+                    }
+                    $comman_key =array('?','這到底怎麼用啦','目錄','舊約','新約','我要抽');
+                    if(in_array($message['text'],$comman_key)){
+                        write_log($guestdata['displayName'],$event['source']['userId'],'comman-'.$message['text'],'4');
+                        exit;
+                    }
+                    $status = 0;
+                    $data['sec'] ='';
+                    $data = cheack_arrange($message['text']);
+                    if($data['error'] == '1'){
+                        $client->reply_text($event['replyToken'],$data['msg']);
+                        write_log($guestdata['displayName'],$event['source']['userId'],$message['text'],'0');
+                        exit;
+                    }
+                    if($data['type'] == 'search' ){
+
+                        $data['sec'] = isset($data['sec'])?$data['sec']:'';
+                        $results = search($data['book'],$data['chap'],$data['sec'],$message['text']);
+
+                        if($results['error']!='1' && $results['status']=='1'){
+                            $text_arr = text_change_arr($results['data']);
+                            $client->reply_text_arr($event['replyToken'],$text_arr);
+                        }else if($results['error']!='1' && $results['status']=='2'){
+                            $client->reply_text($event['replyToken'],$results['data']);
+                        }
+
+                    }else if($data['type'] == 'kw' ||$data['type'] == 'kwf'){
+                        $results = search_keyword($data['kw'],$data['type']);
+                        if($results['status']=='ok'){
+                            $status ='1';
+                        }else if($results['status']=='error'){
+                            $status = '6';
+                        }else{
+                            $status = '1';
+                        }
+                            $client->reply_text($event['replyToken'],$results['msg']);
+                    }else{
+                        $text = '意料以外的錯誤，請麻煩通知開發人一下！'.emoji('10007D');
+                        $client->reply_text($event['replyToken'],$text);
+                        $status = '6';
+                    }
+                    write_log($guestdata['displayName'],$event['source']['userId'],$message['text'],$status);
+                    break;
+                default:
+                    write_log($guestdata['displayName'],$event['source']['userId'],'Unsupported message type-'.$message['type'],'5');
+                    error_log('Unsupported message type: ' . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            write_log($guestdata['displayName'],$event['source']['userId'],'Unsupported event type-'.$event['type'],'5');
+            error_log('Unsupported event type: ' . $event['type']);
+            break;
+    }
 };
