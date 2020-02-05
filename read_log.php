f3762ede5847e5a99b579a3c658818aa4b1ebe73
galio <pu758614@gmail.com>
2020-02-06 00:58:56 +0800

    123

--- read_log.php
+++ read_log.php
@@ -1,6 +1,6 @@
 <?php
 
-$file_path = "user_call.log";
+$file_path = "test.log";
 if(file_exists($file_path)){
 $fp = fopen($file_path,"r");
 $str = fread($fp,filesize($file_path));//指定讀取大小，這裡把整個檔案內容讀取出來
