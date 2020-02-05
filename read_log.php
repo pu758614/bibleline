3ef3c5fe8677d5085b7ff02ed507e2d74f3a9846
galio <pu758614@gmail.com>
2020-02-06 01:04:07 +0800

    123

--- read_log.php
+++ read_log.php
@@ -2,9 +2,10 @@
 
 $file_path = "test.log";
 if(file_exists($file_path)){
-$fp = fopen($file_path,"r");
-$str = fread($fp,filesize($file_path));//指定讀取大小，這裡把整個檔案內容讀取出來
-echo $str = str_replace("\r\n","<br />",$str);
+    echo "is_file<br>";
+    $fp = fopen($file_path,"r");
+    $str = fread($fp,filesize($file_path));//指定讀取大小，這裡把整個檔案內容讀取出來
+    echo $str = str_replace("\r\n","<br />",$str);
 }
 
 
