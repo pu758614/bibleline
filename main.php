bfb777f9612ad79066e60f386ff6b37a5b866d55
galio <pu758614@gmail.com>
2020-02-06 02:10:14 +0800

    123

--- main.php
+++ main.php
@@ -21,8 +21,7 @@ include 'bible_list_arr.php';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 //$even = $client->parseEvents();
 //$data = $client->parseEvents();
-pr($arr);
-exit;
+
 foreach ($client->parseEvents() as $event) {
     $guestdata = getGuestInfo($channelAccessToken,$channelSecret,$event['source']['userId']);
     pr($event);
