3db230c5bdcd3faf846113a68a7883c16ca98a5d
pu758614 <pu758614@gmail.com>
2020-02-07 01:23:08 +0800

    Update channel_data.php

--- bible/channel_data.php
+++ bible/channel_data.php
@@ -1,4 +1,4 @@
 <?php
-$channelAccessToken = 'AkgG8rpHCU52bDkGWQkJ0qrcSqfbtzWrfbZKBvwCuycKwvj9hOkngBZ7y4lO8KrF0XJkQ+C5DkG8GpPJufjCV3T5YfT8ddOY6YV/zmN+u7r1WF8lXKyn1Wjhz2fae4Dkq/9iqq6rPo/l7PWN3OKy2gdB04t89/1O/w1cDnyilFU=';
-$channelSecret = 'ad238d55eb81b1fab33eaf8cd0a253ab';
+$channelAccessToken = '';
+$channelSecret = '';
 ?>
