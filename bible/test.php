60c5d0152a240a79030545186bb094e84355adaa
galio <pu758614@gmail.com>
2020-02-08 02:52:15 +0800

    123

--- bible/test.php
+++ bible/test.php
@@ -8,7 +8,7 @@ $passwd = 'afa6a322';
 //資料庫名稱
 $database = 'heroku_4d9bdcbc4d69fab';
 //實例化mysqli(資料庫路徑, 登入帳號, 登入密碼, 資料庫)
-$db = ADONewConnection('mysql');
+$db = ADONewConnection('mysqli');
 //$conn->debug = true;
 $db -> Connect($host,$user,$passwd,$database);
 
