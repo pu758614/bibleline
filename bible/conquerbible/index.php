394a3051112a1694528322eafb2202790b58c24f
galio <pu758614@gmail.com>
2020-02-17 21:32:36 +0800

    make it better

--- bible/conquerbible/index.php
+++ bible/conquerbible/index.php
@@ -3,10 +3,10 @@ header("Content-Type:text/html; charset=utf-8");
 include ('../vendor/autoload.php');
 include ('../../vendor/autoload.php');
 include ('../LINEBotTiny.php');
-
+$channelAccessToken = '1613644531';
+$channelSecret = '9f28c513a8553cca44af6e42637b9f58';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     $client->reply_text($event['replyToken'],$event['message']);
-    
 }
 ?>
