ac66b04f4f15dfd9be9ef3af8a95f22c85dbfc39
galio <pu758614@gmail.com>
2020-02-17 23:50:49 +0800

    make it better

--- bible/conquerbible/index.php
+++ bible/conquerbible/index.php
@@ -6,6 +6,7 @@ include ('../LINEBotTiny.php');
 $channelAccessToken = '1613644531';
 $channelSecret = '9f28c513a8553cca44af6e42637b9f58';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
+echo "12345";
 foreach ($client->parseEvents() as $event) {
     $client->reply_text($event['replyToken'],$event['message']);
 }
