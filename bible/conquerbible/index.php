7cc131be0d05faca4bcff0cb5a4c17098f355db2
galio <pu758614@gmail.com>
2020-02-18 22:25:57 +0800

    make it better

--- bible/conquerbible/index.php
+++ bible/conquerbible/index.php
@@ -5,7 +5,7 @@ include_once('../../vendor/autoload.php');
 include_once("lib/common.php");
 include_once("lib/db_lib.php");
 include_once('../LINEBotTiny.php');
-$channelAccessToken = '1613644531';
+$channelAccessToken = 'W8zsv6O4gUUTZtfhuphcpDtrwim8YMgWu5vBXT0FQgAzJCzIe6IB6nELeSmDZxCQNTB8i3LCmgNYNCH78Em6RKyWJxNwJLZeJaeQ5yw838tp4vqbpl5e5QUJtUn7ZtcE75i9IHvq0XH+Him1EXJxfwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '9f28c513a8553cca44af6e42637b9f58';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 $db_lib = new db_lib;
