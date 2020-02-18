a33a9b89096f40e9c86ba42a62da1d13839975b0
galio <pu758614@gmail.com>
2020-02-18 23:47:06 +0800

    make it better

--- conquerbible/index.php
+++ conquerbible/index.php
@@ -4,27 +4,28 @@ include_once("conf/conf.php");
 include_once('../vendor/autoload.php');
 include_once("lib/common.php");
 include_once("lib/db_lib.php");
-include_once('../lib/LINEBotTiny.php');
-pr($channelAccessToken);
-pr($channelSecret);
+include_once('../LINEBotTiny.php');
+$channelAccessToken = 'W8zsv6O4gUUTZtfhuphcpDtrwim8YMgWu5vBXT0FQgAzJCzIe6IB6nELeSmDZxCQNTB8i3LCmgNYNCH78Em6RKyWJxNwJLZeJaeQ5yw838tp4vqbpl5e5QUJtUn7ZtcE75i9IHvq0XH+Him1EXJxfwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '9f28c513a8553cca44af6e42637b9f58';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-$db = new db_lib;
+$db_lib = new db_lib;
+$msg = $client->parseEvents();
+//pr($msg);
+$data = array(
+    "name_id" => "123456",
+    "name" => "黃世X",
+    "inster_msg" => "123123",
+    "repont_json" => json_encode($msg),
+    "create_time" => date("Y-m-d H:i:s"),
+);
 
+//$db_lib->db->debug = 1;
+$db_lib->insertData('line_bible_log',$data);
+//$db_lib->db->debug = 0;
+// $db->record_msg_log($uuid,$msg);
 
 foreach ($client->parseEvents() as $event) {
-    $uuid = $event['source']['userId'];
-    $msg = $event['message'];
-    //$guestdata = getGuestInfo($channelAccessToken,$channelSecret,$user_id);
-    $db->record_msg_log($uuid,$msg);
 
-    // $user_info = $db->getUserInfo($uuid,'uuid');
-    // if(count($user_info)==0){
-    //     $line_user_result = $db->addLineUser('U7024af33ac34455f97b39b7bee8b8436');
-    //     if($line_user_result){
-    //         $user_info = $db->getUserInfo($line_user_result,'uuid');
-    //     }
-    // }
-
-    $client->reply_text($event['replyToken'],$msg);
+    $client->reply_text($event['replyToken'],"123456");
 }
 ?>
