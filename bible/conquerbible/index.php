d68aabd8ab4289851dec3d5f8d71427784840dcf
galio <pu758614@gmail.com>
2020-02-18 21:33:34 +0800

    make it better

--- bible/conquerbible/index.php
+++ bible/conquerbible/index.php
@@ -9,11 +9,18 @@ include ('../LINEBotTiny.php');
 $channelAccessToken = '1613644531';
 $channelSecret = '9f28c513a8553cca44af6e42637b9f58';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
+$db = new db_lib;
+$msg = $client->parseEvents();
 
+$data = array(
+    "repont_json" => json_encode($msg),
+    "create_time" => date("Y-m-d H:i:s"),
+);
+$db->insertData('line_bible_log',$data);
 // $db->record_msg_log($uuid,$msg);
 
 foreach ($client->parseEvents() as $event) {
 
-    $client->reply_text($event['replyToken'],"123456");
+    //$client->reply_text($event['replyToken'],"123456");
 }
 ?>
