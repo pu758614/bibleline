8cd899c8acba69bc32979bf04b41deed185c6b6d
galio <pu758614@gmail.com>
2020-02-22 00:13:33 +0800

    make it better

--- conquerbible/index.php
+++ conquerbible/index.php
@@ -17,7 +17,8 @@ $status = 0;
 foreach ($client->parseEvents() as $event) {
     $uuid = $event['source']['userId'];
     $msg = $event['message']['text'];
-    $action = convertStrType($msg);
+    $msg = trim($msg)
+    $msg = convertStrType($msg);
     $msg_log_id = $db->record_msg_log($uuid,file_get_contents('php://input'));
     $user_info = $db->getUserInfo($uuid,'uuid');
     if(count($user_info)==0){
