4ca96c3ecac301fe2efe376631354ac37b696f3f
galio <pu758614@gmail.com>
2020-02-29 23:07:03 +0800

    first commit

--- conquerbible/test.php
+++ conquerbible/test.php
@@ -5,14 +5,19 @@ include_once("lib/common.php");
 include_once("lib/db_lib.php");
 
 $db = new db_lib;
-$data=array(
-    "name" => '帖撒羅尼迦後書',
-);
-$db->db->debug = 1;
-$result=$db->updateData('conquer_bible_book',$data,array("id"=>53));
-$db->db->debug = 0;
-pr($result);
-$cond = array("testament"=>1);
-$data = $db->getArrayByArray('conquer_bible_book',$cond);
-pr($data);
+
+$a = getDecodeStr('QnkwLytVaEl2c3M9');
+echo $a;
+$b = getEncodeStr('');
+echo $b;
+// $data=array(
+//     "name" => '帖撒羅尼迦後書',
+// );
+// $db->db->debug = 1;
+// $result=$db->updateData('conquer_bible_book',$data,array("id"=>53));
+// $db->db->debug = 0;
+// pr($result);
+// $cond = array("testament"=>1);
+// $data = $db->getArrayByArray('conquer_bible_book',$cond);
+// pr($data);
 ?>
